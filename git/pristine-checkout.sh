d350b6397cc83a8551ae19d4450d9fec6f514f1b
Jordan Sissel <jls@semicomplete.com>
2011-04-22 16:19:21 -0700

    - error checking + better output

--- git/pristine-checkout.sh
+++ git/pristine-checkout.sh
@@ -2,6 +2,17 @@
 branch=$1
 destination=$2
 
+basedir=$(dirname $0)
+
+if [ -z "$branch" -o -z "$destination" ] ; then
+  echo "Usage: $0 <branch> <destination>"
+  exit 1
+fi
+
+echo "git fetch..."
 git fetch
+
 # XXX git archive --prefix vs tar -C, which one is better?
+
+echo "Putting pristine copy of $(sh $basedir/upstream.sh $branch) in $destination"
 git archive --format=tar --prefix=$destination/ $branch | tar -xf -
