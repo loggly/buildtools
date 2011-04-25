2242fe44309b3040dc70bca97370c84f9acbfab8
Mike Blume <mike@loggly.com>
2011-04-25 22:09:20 +0000

    make sure to use Commit Date, not Author Date

--- git/last-commit-timestamp.sh
+++ git/last-commit-timestamp.sh
@@ -3,4 +3,4 @@ branch=$1
 basedir=$(dirname $0)
 [ -z "$branch" ] && branch=$(sh $basedir/current-branch.sh)
 
-git log --date=raw -n 1 $branch | awk '/Date:/ { print $2 + (60 * substr($1, 1, 3)) }'
+git log --date=raw -n 1 --pretty=fuller $branch | awk '/CommitDate:/ { print $2 + (60 * substr($1, 1, 3)) }'
