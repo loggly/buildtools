c78245f8d48af37e5a6689ff11e7869c7d9af37c
Jordan Sissel <jls@semicomplete.com>
2011-10-07 13:12:10 -0700

    - add workaround for jenkins

--- git/current-branch.sh
+++ git/current-branch.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-git branch | egrep '^\*' | cut -b '3-'
+branch="$(git branch | egrep '^\*' | cut -b '3-')"
+
+# Jenkins' Git plugin uses detached heads with no branch, but it
+# also sets GIT_BRANCH in environment.
+if [ "$branch" = "(no branch)" ] ; then
+  branch=$GIT_BRANCH
+fi
+
+if [ "$branch" = "" ] ; then
+  branch="unknown-branch"
+fi
+
+echo "$branch"
